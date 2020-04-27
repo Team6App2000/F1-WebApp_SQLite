76e70734090e9184bad349df936f865319ccef8b
Stian <35334952+Dokixi@users.noreply.github.com>
2020-04-27 16:18:03 +0200

    Tutorial (Del 5 og 6)
    Utført del 5 og 6 av web app tutorialen om hvordan man setter opp en lokal database.
    
    Del 5:
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/da1?view=aspnetcore-3.1
    
    Del 6:
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/search?view=aspnetcore-3.1

--- F1-WebApp/Models/Movie.cs
+++ F1-WebApp/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace F1_WebApp.Models
 {
@@ -8,9 +9,12 @@ namespace F1_WebApp.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
